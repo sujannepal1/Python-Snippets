58a43036f723320bf0758eebe29d47d039a2e54f
Sujan Nepal <sujan.nepal@8squarei.com>
2024-10-13 21:34:07 +0545

    feat: add more syntax from  Łukasz Langa - code::dive 2023

--- generators/simple_generators.py
+++ generators/simple_generators.py
@@ -1,3 +1,6 @@
+from typing import Iterator
+
+
 class SimpleGenerator:
     # Two different syntax for simple generators
 
@@ -19,6 +22,7 @@ class SimpleGenerator:
     @classmethod
     def print_generate_value(cls):
         generator_ = cls.generate_value(3)
+        print("Value for generate_value in print_generate_value")
         for value in generator_:
             print(value)
 
@@ -26,13 +30,42 @@ class SimpleGenerator:
     @classmethod
     def print_generated_object(cls):
         square_generator_ = cls.generator_object(3)
-        print(square_generator_)  # return generator object
+        # print(square_generator_)  # return generator object
+        print("Value for generator_object in print_generated_object")
         for i in square_generator_:
             print(i)
 
+    @classmethod
+    def splat_operator_for_unpacking_generator(cls):
+        # as of python 3.5
+        print("Value for splat operator")
+        print(*(x for x in range(10)))
 
-SimpleGenerator.print_generate_value()
-SimpleGenerator.print_generated_object()
+    @classmethod
+    def gen(cls, skip_even: bool = False) -> Iterator[int]:
+        yield 1
+        if not skip_even:
+            yield 2
+        yield 3
+
+    @classmethod
+    def print_gen(cls):
+        print("Value for gen using print_gen")
+        g = cls.gen(skip_even=True)
+        print(next(g))
+        print(next(g))
+        try:
+            print(next(g))
+        except StopIteration:
+            # todo add specific exception
+            print("Only two value")
+
+
+class_ = SimpleGenerator
+class_.print_generate_value()
+class_.print_generated_object()
+class_.splat_operator_for_unpacking_generator()
+class_.print_gen()
 
 # Note: The syntax of generator is same as tuple comprehension
 
