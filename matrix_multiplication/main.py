9c7fbfb690ad98f6613f93634c460af3bdb7aa48
Sujan Nepal <sujan.nepal@8squarei.com>
2024-11-14 16:37:58 +0545

    feat: comments for matrix multiplication

--- matrix_multiplication/main.py
+++ matrix_multiplication/main.py
@@ -7,10 +7,9 @@ size_of_b = f"{len(b)} x {len(b[0])}"
 print(f"a is a {size_of_a} matrix")
 print(f"b is a {size_of_b} matrix")
 
-# multiple a * b
+# result of a * b
 result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-# iterate through rows of a
 result = [
     [sum(a * b for a, b in zip(X_row, Y_col)) for Y_col in zip(*b)] for X_row in a
 ]
